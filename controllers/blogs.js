e92aac17f9e24336366f770f9676a44b70990be2
Dinesh-Khanal <dinesh_Khanal@hotmail.com>
2022-07-11 12:16:29 +0545

    Exercise up to 4.14 completed

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -6,9 +6,31 @@ router.get("/", async (request, response) => {
 });
 
 router.post("/", async (request, response) => {
-  const blog = new Blog(request.body);
-
-  const result = await blog.save();
-  response.status(201).json(result);
+  if (!request.body.title || !request.body.url) {
+    response.status(400).end();
+  } else {
+    const blog = new Blog(request.body);
+    const result = await blog.save();
+    response.status(201).json(result);
+  }
+});
+router.delete("/:id", async (request, response, next) => {
+  try {
+    await Blog.findByIdAndRemove(request.params.id);
+    response.status(204).end();
+  } catch (error) {
+    next(error);
+  }
+});
+router.put("/:id", async (request, response, next) => {
+  try {
+    const blog = request.body;
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+      new: true,
+    });
+    response.json(updatedBlog);
+  } catch (error) {
+    next(error);
+  }
 });
 module.exports = router;
--- test/api_blogs.test.js
+++ test/api_blogs.test.js
@@ -49,7 +49,6 @@ test("A valid blog can be added", async () => {
     url: "http://ramesh.blogspot.com",
     likes: 2,
   };
-  const blog = new Blog(newBlog);
   await api
     .post("/api/blogs")
     .send(newBlog)
@@ -60,6 +59,69 @@ test("A valid blog can be added", async () => {
   const titles = response.body.map((r) => r.title);
   expect(titles).toHaveLength(initialBlogs.length + 1);
   expect(titles).toContain("Jest testing learning is fun");
+});
+test("like property is not missing", async () => {
+  const newBlog = {
+    title: "Jest testing learning is fun",
+    author: "Ramesh Mainali",
+    url: "http://ramesh.blogspot.com",
+    likes: 0,
+  };
+  const request = await api.post("/api/blogs").send(newBlog);
+  expect(request.body.likes).toBeDefined();
+});
+describe("title and url are not missing", () => {
+  test("title missing", async () => {
+    const newBlog = {
+      author: "Ramesh Mainali",
+      url: "http://ramesh.blogspot.com",
+      likes: 0,
+    };
+    await api.post("/api/blogs").send(newBlog).expect(400);
+  }, 100000);
+  test("url missing", async () => {
+    const newBlog = {
+      title: "Jest testing learning is fun",
+      author: "Ramesh Mainali",
+      likes: 0,
+    };
+    await api.post("/api/blogs").send(newBlog).expect(400);
+  }, 100000);
+  test("title and url are not missing", async () => {
+    const newBlog = {
+      title: "Jest testing learning is fun",
+      author: "Ramesh Mainali",
+      url: "http://ramesh.blogspot.com",
+      likes: 0,
+    };
+    await api.post("/api/blogs").send(newBlog).expect(201);
+  }, 100000);
+});
+
+describe("Deleting and updating blog post", () => {
+  test("Deleting single blog post", async () => {
+    const blogsAtStart = await Blog.find();
+    const blogToDelete = blogsAtStart[0];
+    await api.delete(`/api/blogs/${blogToDelete.id}`);
+    expect(204);
+    const blogsAtEnd = await Blog.find();
+    expect(blogsAtEnd).toHaveLength(initialBlogs.length - 1);
+    const titles = blogsAtEnd.map((b) => b.title);
+    expect(titles).not.toContain(blogToDelete.title);
+  });
+  test("Updating single blog post", async () => {
+    const blogsAtStart = await Blog.find();
+    const blogToUpdate = blogsAtStart[0];
+    const updateTo = {
+      title: "Jest testing learning is fun",
+      author: "Ramesh Mainali",
+      url: "http://ramesh.blogspot.com",
+      likes: 4,
+    };
+    await api.put(`/api/blogs/${blogToUpdate.id}`).send(updateTo).expect(200);
+    const blogAfterUpdate = await Blog.findById({ _id: blogToUpdate.id });
+    expect(blogAfterUpdate.likes).toBe(4);
+  });
 });
 afterAll(() => {
   mongoose.connection.close();
